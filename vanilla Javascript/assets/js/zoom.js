f3ea56c7808825897145c5476569d2653c049923
KIM-DONGJU <58763448+KIM-DONGJU@users.noreply.github.com>
2021-06-18 18:16:13 +0900

    mouse hover event 작성중, layer로 띄우는 부분 javascript 아직 구현하지 못함

---
+++ vanilla Javascript/assets/js/zoom.js
@@ -0,0 +1,9 @@
+function zoomIn(event) {
+  event.target.style.width = "120%";
+  event.target.style.transition = "all 0.5s";
+}
+
+function zoomOut(event) {
+  event.target.style.width = "100%";
+  event.target.style.transition = "all 0.5s";
+}
