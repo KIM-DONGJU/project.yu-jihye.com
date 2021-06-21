c02210812584682c5b2a797783744c1fed688d9a
KIM-DONGJU <58763448+KIM-DONGJU@users.noreply.github.com>
2021-06-21 16:48:58 +0900

    slide 개수 상관없이 반영되도록 수정중

--- vanilla Javascript/assets/js/slider.js
+++ vanilla Javascript/assets/js/slider.js
@@ -1,61 +1,69 @@
 document.querySelector(".next-button").addEventListener("click", () => {
-    let contents = document.querySelector(".contents");
+  let contents = document.querySelector(".contents");
 
-    if (contents.style.transform === "translate(-50%)") {
-        contents.style.transform = "translate(-75%)";
-        setTimeout(() => {
-            contents.style.transition = "none";
-            contents.style.transform = "translate(-25%)"
-        }, 300);
-    } else {
-        contents.style.transition = "0.3s";
-        contents.style.transform = "translate(-50%)";
-    }
-})
+  if (contents.style.transform === "translate(-50%)") {
+    contents.style.transform = "translate(-75%)";
+    setTimeout(() => {
+      contents.style.transition = "none";
+      contents.style.transform = "translate(-25%)";
+    }, 300);
+  } else {
+    contents.style.transition = "0.3s";
+    contents.style.transform = "translate(-50%)";
+  }
+});
 
 document.querySelector(".previous-button").addEventListener("click", () => {
-    let contents = document.querySelector(".contents");
+  let contents = document.querySelector(".contents");
 
-    if (contents.style.transform === "translate(-25%)" || contents.style.transform === "") {
-        contents.style.transition = "0.3s";
-        contents.style.transform = "translate(0)";
-        setTimeout(() => {
-            contents.style.transition = "none";
-            contents.style.transform = "translate(-50%)"
-        }, 300);
-    } else {
-        contents.style.transition = "0.3s";
-        contents.style.transform = "translate(-25%)";
-    }
-})
+  if (
+    contents.style.transform === "translate(-25%)" ||
+    contents.style.transform === ""
+  ) {
+    contents.style.transition = "0.3s";
+    contents.style.transform = "translate(0)";
+    setTimeout(() => {
+      contents.style.transition = "none";
+      contents.style.transform = "translate(-50%)";
+    }, 300);
+  } else {
+    contents.style.transition = "0.3s";
+    contents.style.transform = "translate(-25%)";
+  }
+});
 
 document.querySelector(".small-next-button").addEventListener("click", () => {
+  let contents = document.querySelector(".logo-contents");
+
+  if (contents.style.transform === "translate(-50%)") {
+    contents.style.transform = "translate(-75%)";
+    setTimeout(() => {
+      contents.style.transition = "none";
+      contents.style.transform = "translate(-25%)";
+    }, 300);
+  } else {
+    contents.style.transition = "0.3s";
+    contents.style.transform = "translate(-50%)";
+  }
+});
+
+document
+  .querySelector(".small-previous-button")
+  .addEventListener("click", () => {
     let contents = document.querySelector(".logo-contents");
 
-    if (contents.style.transform === "translate(-50%)") {
-        contents.style.transform = "translate(-75%)";
-        setTimeout(() => {
-            contents.style.transition = "none";
-            contents.style.transform = "translate(-25%)"
-        }, 300);
-    } else {
-        contents.style.transition = "0.3s";
+    if (
+      contents.style.transform === "translate(-25%)" ||
+      contents.style.transform === ""
+    ) {
+      contents.style.transition = "0.3s";
+      contents.style.transform = "translate(0)";
+      setTimeout(() => {
+        contents.style.transition = "none";
         contents.style.transform = "translate(-50%)";
-    }
-})
-
-document.querySelector(".small-previous-button").addEventListener("click", () => {
-    let contents = document.querySelector(".logo-contents");
-
-    if (contents.style.transform === "translate(-25%)" || contents.style.transform === "") {
-        contents.style.transition = "0.3s";
-        contents.style.transform = "translate(0)";
-        setTimeout(() => {
-            contents.style.transition = "none";
-            contents.style.transform = "translate(-50%)"
-        }, 300);
+      }, 300);
     } else {
-        contents.style.transition = "0.3s";
-        contents.style.transform = "translate(-25%)";
+      contents.style.transition = "0.3s";
+      contents.style.transform = "translate(-25%)";
     }
-})
+  });
